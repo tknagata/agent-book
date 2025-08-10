8668826fb016ea9dfa30ecb632a0633a564a98d6
tubone <tubo.yyyuuu@gmail.com>
2025-08-10 22:26:14 +0900

    ハンズオンに合わせて、インデント調整

--- chapter5/5-4-3/amplify/backend.ts
+++ chapter5/5-4-3/amplify/backend.ts
@@ -1,17 +1,14 @@
-import {defineBackend} from "@aws-amplify/backend";
-import {auth} from "./auth/resource";
-import {ManagedPolicy} from "aws-cdk-lib/aws-iam";
-
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { ManagedPolicy } from "aws-cdk-lib/aws-iam";
 
 const backend = defineBackend({
   auth,
 });
 
-
 // 認証済みユーザーのIAM Roleを取得
 const authenticatedUserIamRole =
   backend.auth.resources.authenticatedUserIamRole;
-
 
 // Bedrock用のAWSマネージドポリシーを追加
 authenticatedUserIamRole.addManagedPolicy(
