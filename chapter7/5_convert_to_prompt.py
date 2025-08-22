8849d2842edd7c3b5d5ea8c53022580a96ccc656
TSUKADA Masaki <licuxyx5z@gmail.com>
2025-08-23 06:09:14 +0900

    変数名のtypo修正

--- chapter7/5_convert_to_prompt.py
+++ chapter7/5_convert_to_prompt.py
@@ -8,5 +8,5 @@ langfuse = get_client()
 prompt_template = langfuse.get_prompt("ai-agent", type="chat", label="latest")
 
 langchain_prompt = ChatPromptTemplate(prompt_template.get_langchain_prompt())
-messagses = langchain_prompt.invoke({"city": "東京都"})
+messages = langchain_prompt.invoke({"city": "東京都"})
 print(messagses)
