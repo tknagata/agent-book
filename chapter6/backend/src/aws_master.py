ae20eeb5bcd6dfb3cfc726832df41f6d58b36c0b
minorun365 <mail@minoruonda.com>
2025-08-31 22:25:21 +0900

    更新

--- chapter6/backend/src/aws_master.py
+++ chapter6/backend/src/aws_master.py
@@ -1,6 +1,6 @@
 import asyncio
 from strands import Agent, tool
-from strands.tools.mcp.mcp_client import MCPClient
+from strands.tools.mcp import MCPClient
 from mcp.client.streamable_http import streamablehttp_client
 from .agent_executor import invoke
 
