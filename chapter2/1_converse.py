ddbef572e18d68a9e524b55dc4147c3640086f2e
Takayuki NAGATA <nagata@arithink.com>
2025-10-17 10:34:06 +0000

    modify modelid

--- chapter2/1_converse.py
+++ chapter2/1_converse.py
@@ -6,18 +6,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Bedrock呼び出し用のAPIクライアントを作成
-client = boto3.client("bedrock-runtime")
+client = boto3.client("bedrock-runtime", region_name="us-west-2")
 
 # Converse APIを実行
 response = client.converse(
     # modelId="us.anthropic.claude-3-7-sonnet-20250219-v1:0", # モデルID
-    modelId="anthropic.claude-sonnet-4-5-20250929-v1:0", # モデルID
+    modelId="arn:aws:bedrock:us-west-2:201189646499:inference-profile/global.anthropic.claude-sonnet-4-20250514-v1:0", # モデルID
     messages=[{
         "role": "user",
         "content": [{
             "text": "こんにちは" # 入力メッセージ
         }]
-    }]
+    }],
+    inferenceConfig={"maxTokens": 512, "temperature": 0.5, "topP": 0.9}
 )
 
 # 実行結果のテキストだけを画面に表示
